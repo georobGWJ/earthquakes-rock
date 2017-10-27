e4e2d5860a97f8c6730406766ed0fbb06bd57a63
Robert Turner <r.turner@fugro.com>
2017-10-27 11:04:04 -0700

    Create skeleton GeoPoint utility classes and tests.

---
+++ util/geo_point_UTM.py
@@ -0,0 +1,29 @@
+import math
+import unittest
+
+class GeoPointUTM(GeoPoint):
+    """ A class to represent a location on Earth using latitude and longitude. """
+
+    def __str__:
+        pass
+
+    def horiz_distance_to_point:
+        """ Return horizontal distance from self to another GeoPoint """
+        pass
+
+    def abs_distance_to_point:
+        """ Return absolute distance from self to another GeoPoint """
+        pass
+    
+
+
+    
+class TestEvent(unittest.TestCase):
+    """ Unit tests for the GeoPoint class. """
+
+    def test_init(self):
+        self.assertTrue(0 == 0)
+        self.assertFalse(42 == 'Mw')
+
+if __name__ == '__main__':
+    unittest.main()
